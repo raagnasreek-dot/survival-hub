4eb4eb12bc72be9331021591c598132fe5419716
raagnasreek-dot <raagnasreek@gmail.com>
2026-08-31 20:45:51 +0530

    Delete config.py

--- config.py
+++
@@ -1,6 +0,0 @@
-import os
-
-DB_HOST = os.getenv("DB_HOST", "localhost")
-DB_USER = os.getenv("DB_USER", "root")
-DB_PASSWORD = os.getenv("DB_PASSWORD", "")
-DB_NAME = os.getenv("DB_NAME", "survival_hub")
